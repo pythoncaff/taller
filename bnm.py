553032edad925fc5722f51e281d93e13af19401c
Diego de la Hera <delahera@gmail.com>
2016-11-12 12:07:28 -0300

    implementamos la clase RegistrosDB

--- bnm.py
+++ bnm.py
@@ -23,6 +23,37 @@ import pdb
 import sqlite3
 
 
+class RegistrosDB():
+
+    def __init__(self, database):
+        self._connect_db(database)
+        self._create_table()
+
+    def _connect_db(self, database):
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def _create_table(self):
+        self.cursor.execute(''' CREATE TABLE IF NOT EXISTS records
+                        (id INTEGER PRIMARY KEY,
+                        server_response BOOLEAN NOT NULL,
+                        reg_exists BOOLEAN NOT NULL,
+                        reg_saved BOOLEAN NOT NULL )''')
+
+    def disconnect_db(self):
+        self.connection.close()
+
+    def get_record(self, record):
+        record = (record,)  # creamos una tupla de un elemento
+        self.cursor.execute('SELECT server_response, reg_exists, reg_saved FROM records WHERE id =?', record)
+        return(self.cursor.fetchone())
+
+    def save_record(self, record, server_response, reg_exists, reg_saved):
+        db_insert = (record, server_response, reg_exists, reg_saved)
+        self.cursor.execute('INSERT INTO records VALUES (?, ?, ?, ?)',
+                            db_insert)
+        self.connection.commit()
+
 def unmarcxml(registro_inicial,
               limite,
               url_base='http://www.bnm.me.gov.ar/catalogo/Record/'):
@@ -40,62 +71,52 @@ def unmarcxml(registro_inicial,
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    db_file = 'registro.sqlite'
-    db_connection = sqlite3.connect(directory + "/" + db_file)
-    db_cursor = db_connection.cursor()
-    # PRIMARY KEY es un valor que es unico en la base de datos
-    db_cursor.execute(''' CREATE TABLE IF NOT EXISTS records
-                          (id INTEGER PRIMARY KEY,
-                           server_response BOOLEAN NOT NULL,
-                           reg_exists BOOLEAN NOT NULL,
-                           reg_saved BOOLEAN NOT NULL )''')
+    db_file = directory + "/" + 'registro.sqlite'
+    database = RegistrosDB(db_file)
 
     while registro_actual <= limite:
+        db_response = database.get_record(registro_actual)
         url_final = '%s%09d%s' % (url_base, registro_actual, clave_xml)
-        print('descargando ' + url_final, end=' ')
-        registro_bnm = requests.get(url_final)
-        status = registro_bnm.status_code
-        print(status, end=' ')
-        # Asumimos que todos los estados son false
-        server_response = 0
-        reg_exists = 0
-        reg_saved = 0
-        
-        if status == requests.codes.ok:
-            # ejecutar si código de respuesta OK
-            server_response = 1
-            find_attribute_return = FindAttribute(registro_bnm.text,
-                                                  tag_name,
-                                                  attribute_name,
-                                                  attribute_value)
-            if find_attribute_return == "Guardo":
-                reg_exists = 1
-                reg_saved = 1
-                with open('%s/%09d.xml' % (directory, registro_actual),
-                          'xb') as f:
-                    f.write(registro_bnm.content)
-                lista_registros_bnm.append(registro_bnm)
-                print('Guardado')
-            elif find_attribute_return == "Descarto":
-                reg_exists = 1
+        if db_response is None:
+            print('descargando ' + url_final, end=' ')
+            registro_bnm = requests.get(url_final)
+            status = registro_bnm.status_code
+            print(status, end=' ')
+            # Asumimos que todos los estados son false
+            server_response = 0
+            reg_exists = 0
+            reg_saved = 0
 
+            if status == requests.codes.ok:
+                # ejecutar si código de respuesta OK
+                server_response = 1
+                find_attribute_return = findAttribute(registro_bnm.text,
+                                                      tag_name,
+                                                      attribute_name,
+                                                      attribute_value)
+                if find_attribute_return == "Guardo":
+                    reg_exists = 1
+                    reg_saved = 1
+                    with open('%s/%09d.xml' % (directory, registro_actual),
+                              'xb') as f:
+                        f.write(registro_bnm.content)
+                    lista_registros_bnm.append(registro_bnm)
+                    print('Guardado')
+                elif find_attribute_return == "Descarto":
+                    reg_exists = 1
+            else:
+                print()
+            database.save_record(registro_actual, server_response, reg_exists, reg_saved)
         else:
-            print()
-
-        db_insert = (registro_actual, server_response, reg_exists, reg_saved)
-        db_cursor.execute('INSERT INTO records VALUES (?, ?, ?, ?)',
-                          db_insert)
+            print(url_final + ' ya había sido intentada; sigo a la siguiente...')
 
         registro_actual += 1
-
-        db_connection.commit()
-
-    db_connection.close()
+    database.disconnect_db()
 
     return lista_registros_bnm
 
 
-def FindAttribute(document, tag_name, attribute_name, attribute_value):
+def findAttribute(document, tag_name, attribute_name, attribute_value):
     import xml
     from xml.dom.minidom import parseString
     #pdb.set_trace()
@@ -111,7 +132,6 @@ def FindAttribute(document, tag_name, attribute_name, attribute_value):
         print('Inexistente')
         return "Error"
 
-
 def main(args):
     if len(args) == 4:
         unmarcxml(int(args[1]), int(args[2]), args[3])
@@ -121,7 +141,6 @@ def main(args):
         print("Usage: " + args[0] + " registro_inicial(int) limite(int) [url]")
 
     return 0
-
 
 if __name__ == '__main__':
     import sys
